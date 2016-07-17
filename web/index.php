db19b1be54c5aca88439448496fe41bd83c57029
HizkiFW <hizkifw@gmail.com>
2016-07-17 17:43:43 +0700

    Added directory for webserver

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>nanoworld</title>
+		<style>
+			body {
+				color: #333;
+				font-family: "Calibri", "Arial", sans-serif;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<center>
+			<h1>nanoworld</h1>
+			<p>Coming soon</p>
+		</center>
+	</body>
+</html>
