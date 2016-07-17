7dda205e117378808b3680b99872d836169d8026
HizkiFW <hizkifw@gmail.com>
2016-07-17 18:01:56 +0700

    Updated for refactoring

--- tests/design.php
+++ tests/design.php
@@ -9,32 +9,32 @@ unset($files[0], $files[1]);
 		<script src="nanorender.js"></script>
 		<script>
 			function init() {
-				Nano.init(document.getElementById("canvas"));
-				Nano.Characters.create("Test", {body: 1, head: 1, eyes: 1, mouth: 1, hair: 1, arms: 1, legs: 1}, [], [125, 125], 1);
+				Nano.Render.init(document.getElementById("canvas"));
+				Nano.Render.Characters.create("Test", {body: 1, head: 1, eyes: 1, mouth: 1, hair: 1, arms: 1, legs: 1}, [], [125, 125], 1);
 			}
 			function part(p, n) {
-				var x = Nano.Characters.get("Test");
+				var x = Nano.Render.Characters.get("Test");
 				x.parts[p] = n;
-				Nano.characters.Test = x;
+				Nano.Render.characters.Test = x;
 			}
 			
 			var nn = 0;
 			function spawnMany(n) {
 				for(var i = 0; i < n; i++) {
 					nn++;
-					Nano.Characters.create("c" + nn, {hair:(Math.random()>0.5?1:2),eyes:Math.floor(Math.random() * 4)+1}, [], [Math.floor(Math.random() * 250), Math.floor(Math.random() * 250)], (Math.random()>0.5?1:-1));
+					Nano.Render.Characters.create("c" + nn, {hair:(Math.random()>0.5?1:2),eyes:Math.floor(Math.random() * 4)+1}, [], [Math.floor(Math.random() * 250), Math.floor(Math.random() * 250)], (Math.random()>0.5?1:-1));
 				}
 			}
 			
 			function fpstest() {
-				if(Nano.currFps < 25) return;
+				if(Nano.Render.currFps < 25) return;
 				spawnMany(10);
 				dance();
 				setTimeout(fpstest, 500);
 			}
 			function dance() {
-				for(var name in Nano.Characters.list()) {
-					Nano.Characters.faceAway(name);
+				for(var name in Nano.Render.Characters.list()) {
+					Nano.Render.Characters.faceAway(name);
 				}
 			}
 			
@@ -53,8 +53,8 @@ unset($files[0], $files[1]);
 		<div id="canvasArea">
 			<canvas id="canvas" width="250" height="250"></canvas>
 		</div>
-		<button onclick="Nano.Characters.face('Test', -1)">Left</button>
-		<button onclick="Nano.Characters.face('Test', 1)">Right</button>
+		<button onclick="Nano.Render.Characters.face('Test', -1)">Left</button>
+		<button onclick="Nano.Render.Characters.face('Test', 1)">Right</button>
 		<button onclick="spawnMany(1)">Spawn</button>
 		<button onclick="spawnMany(100)">Spawn 100</button>
 		<button onclick="fpstest()">FPS Test</button>
